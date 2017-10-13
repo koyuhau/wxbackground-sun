7a7ccf3a8d394a469d097a67009a2d507339cfd2
gaoyuheng <gaoyuheng@qtone.cn>
2017-10-13 18:03:05 +0800

    video接口增加根据id分类功能

--- client/config.js
+++ client/config.js
@@ -16,7 +16,7 @@ var config = {
 
         // 测试的请求地址，用于测试会话
         //requestUrl: `${host}/weapp/user`,
-        requestUrl: `${host}/weapp/demo`,
+        requestUrl: `${host}/weapp/video?category=1`,
 
         // 测试的信道服务地址
         tunnelUrl: `${host}/weapp/tunnel`,
--- server/controllers/video.js
+++ server/controllers/video.js
@@ -2,7 +2,13 @@ const { mysql } = require('../qcloud')
 
 module.exports = async ctx => {
 
-  var resa = await mysql("video")
+  var query = ctx.request.query
+  if (query.category != null){
+    var resa = await mysql("video").where("category_id", query.category)
+  }else{
+    var resa = await mysql("video")
+  }
+
   ctx.state.data = {
     msg: resa
   }
